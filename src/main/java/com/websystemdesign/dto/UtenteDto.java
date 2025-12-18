823a7533bfc8f6568c3a46e63d027d5050b8b02f
KoganeNoShin <diecoro2003@gmail.com>
2025-12-18 23:26:22 +0100

    Aggiornato utente

--- src/main/java/com/websystemdesign/dto/UtenteDto.java
+++ src/main/java/com/websystemdesign/dto/UtenteDto.java
@@ -1,10 +1,11 @@
 package com.websystemdesign.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
-@Data // Annotazione di Lombok: genera getter, setter, toString, equals, hashCode
+@Data
 public class UtenteDto {
 
     private Long id;
@@ -12,6 +13,13 @@ public class UtenteDto {
     @NotBlank(message = "L'username non può essere vuoto")
     @Size(min = 3, max = 20, message = "L'username deve avere tra 3 e 20 caratteri")
     private String username;
+
+    // WRITE_ONLY: Jackson accetterà questo campo in input (registrazione),
+    // ma lo ignorerà completamente in output (risposte API).
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @NotBlank(message = "La password non può essere vuota")
+    @Size(min = 8, message = "La password deve avere almeno 8 caratteri")
+    private String password;
 
     @NotBlank(message = "Il nome non può essere vuoto")
     private String nome;

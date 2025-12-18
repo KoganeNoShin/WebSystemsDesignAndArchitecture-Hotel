f3b65f2e444bc9ad70b75d093a0cdb8cd2fd0554
KoganeNoShin <diecoro2003@gmail.com>
2025-12-18 23:18:03 +0100

    Eliminato dal DTO la password

--- src/main/java/com/websystemdesign/dto/UtenteDto.java
+++ src/main/java/com/websystemdesign/dto/UtenteDto.java
@@ -13,11 +13,6 @@ public class UtenteDto {
     @Size(min = 3, max = 20, message = "L'username deve avere tra 3 e 20 caratteri")
     private String username;
 
-    // La password viene gestita separatamente per la registrazione
-    @NotBlank(message = "La password non può essere vuota")
-    @Size(min = 8, message = "La password deve avere almeno 8 caratteri")
-    private String password;
-
     @NotBlank(message = "Il nome non può essere vuoto")
     private String nome;
 
